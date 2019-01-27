8876df36f83acda6689e4c5635534925616ba62c
Emily <emilyksullivan@outlook.com>
2019-01-26 21:58:35 -0500

    getting dashboard items to present id specific waterLevel and airTemp
    need to add touchableopacity and station page next

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -24,11 +24,29 @@ class Dashboard extends Component {
 
     componentWillMount() {
         this.fetchData();
+  // [8575512, 1611400, 1612340, 1612480].map(id => this.getDetails(id))
+    //    this.getDetails();
     }
+
+    getDetails = async (id, index)=> {
+        // console.log(id, index)
+        // console.log(this.waterLevelUrlById(id))
+        // console.log(this.airTempUrlById(id))
+        let wl = await(await fetch(this.waterLevelUrlById(id))).json()
+        let at = await(await fetch(this.airTempUrlById(id))).json()
+        console.log(wl, at)
+        let data = this.state.data;
+        data[index].waterLevel = wl.data[0].v
+        data[index].airTemp = at.data[0].v
+        this.setState({data});
+    }
+
+    waterLevelUrlById = (id) => `https://tidesandcurrents.noaa.gov/api/datagetter?&station=${id}&date=latest&units=english&datum=MLLW&product=water_level&time_zone=LST_LDT&format=json&application=NOS.COOPS.TAC.COOPSMAP&interval=`
+    airTempUrlById = (id) => `https://tidesandcurrents.noaa.gov/api/datagetter?&station=${id}&date=latest&units=english&datum=MLLW&product=air_temperature&time_zone=LST_LDT&format=json&application=NOS.COOPS.TAC.COOPSMAP&interval=`
 
     fetchData = async () => {
         //fetch api for ALL water level stations
-        const allTidePredStationsResponse = await fetch("https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations.json?type=tidepredictions");
+        const allTidePredStationsResponse = await fetch("https://tidesandcurrents.noaa.gov/mdapi/v1.0/webapi/stations.json?type=1minute");
         //convert to json
         const json = await allTidePredStationsResponse.json();
         //weed out the other state water level stations
@@ -44,11 +62,13 @@ class Dashboard extends Component {
         // //convert THOSE to jsons
         // const waterLevelJson = await waterLevelNowResponse.json();
         // const airTempJson = await airTempNowResponse.json();
+      //  filteredByMaryland = Promise.all(filteredByMaryland.map((item) => this.getData(item)));
 
         this.setState({ data: filteredByMaryland, 
             //waterLevelData: waterLevelJson.data, airTempData: airTempJson 
             refreshing: false });
     };
+
 
     handleRefresh = () => {
         this.setState({
@@ -105,6 +125,19 @@ class Dashboard extends Component {
     };
 
 
+    listItem = (item, index) => {
+        console.log(item.id)
+        const data = this.getDetails(item.id, index);
+        return (
+            <ListItem
+            title={item.name}
+            subtitle={`Water Level: ${item.waterLevel} | Air Temp: ${item.airTemp}`}
+            containerStyle={{ borderBottomWidth: 0, marginTop: 30, marginBottom: 30}}
+            />
+        )
+    }
+
+
     render() {
         return (
             <View style={{ flex: 1 }}>
@@ -112,15 +145,7 @@ class Dashboard extends Component {
                 {/* <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}> */}
                     <FlatList
                         data={this.state.data}
-                        renderItem={({ item }) => (
-                            <ListItem
-                                title={item.name}
-                                containerStyle={{ borderBottomWidth: 0, marginTop: 30, marginBottom: 30}}
-                            />
-                            // <Text>
-                            //     {item.name}
-                            // </Text>
-                        )}
+                        renderItem={({ item, index }) => this.listItem(item, index)}
                         keyExtractor={item => item.name}
                         ItemSeparatorComponent={this.renderSeparator}
                         ListHeaderComponent={this.renderHeader}
